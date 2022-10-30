a34a916365d5815c6dd1ab749f9aa7725e7b4b2d
GrayWorldFinex <116639741+GrayWorldFinex@users.noreply.github.com>
2022-10-31 00:29:19 +0300

    Add files via upload

---
+++ BitcoinAddressGenerator/GenerateAddress.cs
@@ -0,0 +1,27 @@
+﻿using NBitcoin;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitcoinAddressGenerator
+{
+   class GenerateAddress
+   {
+        public void GenerateBitcoinAddress()
+        {
+            Key privateKey = new Key();
+            BitcoinSecret mainNetPrivateKey = privateKey.GetBitcoinSecret(Network.Main);  // generate our Bitcoin secret(also known as Wallet Import Format or simply WIF) from our private key for the mainnet
+            BitcoinSecret testNetPrivateKey = privateKey.GetBitcoinSecret(Network.TestNet);
+            File.WriteAllText(@"D:\Temp\BitcoinAddressPrivate.txt", mainNetPrivateKey.ToString());
+            if (File.Exists(@"D:\Temp\BitcoinAddressPrivate.txt"))
+            {
+                Process.Start("notepad", @"D:\Temp\BitcoinAddressPrivate.txt");
+            }
+        }
+   }
+
+}
---
+++ BitcoinAddressGenerator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NBitcoin;
+
+namespace BitcoinAddressGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Directory.CreateDirectory(@"D:\Temp");
+            GenerateAddress gen = new GenerateAddress();
+            gen.GenerateBitcoinAddress();
+        }
+    }
+}
